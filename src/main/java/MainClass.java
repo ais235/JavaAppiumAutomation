88de033e3d47103366deb09762a50cc4815ff6e9
Alexandr <optimist235@gmail.com>
2025-02-19 05:05:05 +0300

    реализация testGetClassNumber

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,13 @@
 public class MainClass {
+    public static int class_number = 20;
     public int getLocalNumber()
     {
         int a = 14;
         return a;
     }
+    public int getClassNumber()
+    {
+        return class_number;
+    }
 }
+
--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -6,7 +6,14 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetLocalNumber()
     {
+        //System.out.println(this.getLocalNumber());
         Assert.assertTrue("getLocalNumber != 14", this.getLocalNumber() == 14);
     }
 
+    @Test
+    public void testGetClassNumber()
+    {
+        //System.out.println(this.getClassNumber());
+        Assert.assertTrue("getClassNumber <= 45", this.getClassNumber() > 45);
+    }
 }
